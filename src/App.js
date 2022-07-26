11993cbaae6c5ad04eea48a7a1cf42b330f05a7a
Brian Duong <brianminhduong@gmail.com>
2022-07-25 21:31:42 -0400

    Implement login handler and show header/login if user is logged in

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Counter from './components/Counter';
-
+import React from "react";
+import Counter from "./components/Counter";
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import { useSelector } from "react-redux";
 
 function App() {
+  const show = useSelector((state) => state.auth.isAuthenticated);
   return (
-    <Counter />
+    <>
+      {show && <Header />}
+      {!show && <Auth />}
+      <Counter />
+    </>
   );
 }
 
